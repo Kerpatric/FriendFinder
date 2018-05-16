8b3339bf356f12275f8f0ba7c48d872d56f37602
Eric <eostler13@gmail.com>
2018-05-16 13:44:48 -0600

    publish intial commit

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+
+var friendsData = [
+	{
+		name: 'Dak Dak',
+		photo: 'https://goo.gl/images/D4oVX8',
+		scores: [
+      '1',
+			'2',
+			'3',
+			'4',
+			'5',
+			'1',
+			'2',
+			'3',
+			'4',
+			'5'
+		]
+	},
+	{
+		name: 'Mickey Miiiike',
+		photo: 'https://goo.gl/images/Gy5tbq',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'2',
+			'2',
+			'2',
+			'3',
+			'3'
+		]
+	},
+];
+
+module.exports = friendsData;
